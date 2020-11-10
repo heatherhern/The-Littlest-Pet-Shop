cd5d280d52053906cdae0b5f78e4ad6fa31fa925
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-09 21:05:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 let express = require("express");
-var petfinder = require("@petfinder/petfinder-js");
-var client = new petfinder.Client({ apiKey: "5TkMnekEnJXhvW93ahigxXgDTTRoN6vx6ILaDflK6EXBovIOOD", secret: "KGpQTqYKB3gyq72fi5lCvLJ2r81dEGHdC4jLagfO" });
 
 let PORT = process.env.PORT || 3000;
 
@@ -23,50 +21,6 @@ let routes = require("./controllers/petController.js");
 app.use(routes);
 
 db.sequelize.sync({ force: true }).then(function () {
-    client.animal.search().then(function (response) {
-        let clinicArr = [];
-
-
-        response.data.animals.forEach(element => {
-            let petObj = {
-                name: element.name,
-                url: element.url,
-                species: element.species,
-                age: element.age,
-                gender: element.gender,
-                photo_url: element.photos,
-                organization_id: element.organization_id
-            };
-
-            let clinicObj = {
-                organization_id: element.organization_id,
-                email: element.contact.email,
-                phone: element.contact.phone,
-                address1: element.contact.address.address1,
-                address2: element.contact.address.address2,
-                city: element.contact.address.city,
-                state: element.contact.address.state,
-                zipcode: parseInt(element.contact.address.postcode),
-                country: element.contact.address.country
-            };
-
-            clinicArr.forEach(el => {
-                if (el.organization_id === clinicObj.organization_id) { }
-                else {
-                    clinicArr.push(clinicObj);
-                }
-            });
-        });
-    });
-
-
-
-
-
-
-
-
-
     app.listen(PORT, function () {
         console.log("App listening on PORT " + PORT);
     });
