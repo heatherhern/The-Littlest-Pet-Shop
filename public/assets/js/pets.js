8989f6a9add1d84b1f0e67d29770bb649bc48f1b
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-11 19:38:18 -0600

    Update pets.js

--- public/assets/js/pets.js
+++ public/assets/js/pets.js
@@ -1,1 +1,19 @@
+$('#registerForm').on('submit', (event)=>{
+    event.preventDefault();
+    let registerObj = {
+        name: $('#name').val(),
+        email: $('#email').val(),
+        password: $('#password').val(),
+        agreeToTerms: $('#agreeToTerms').val()
+    };
 
+    if(registerObj.password === $('#confirmPassword').val()){
+        $.post('/api/user/register', registerObj).then(function(response){
+            if(response === 200){
+                //alert('good');
+            }
+        });
+    }else{
+        //alert user of mismatching passwords, not meeting required security
+    }
+});
