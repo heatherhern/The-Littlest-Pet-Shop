b7ad0f2617e8d11b3a63e9efe1faf53fb19f2bf5
Allister Rampenthal <allisterrampenthal@gmail.com>
2020-11-16 18:20:01 -0600

    removing passport

--- server.js
+++ server.js
@@ -3,32 +3,44 @@ const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 const db = require("./models");
-const passport = require("passport");
-const flash = require('connect-flash');
 const cookieParser = require('cookie-parser');
 const session = require("express-session"),
     bodyParser = require("body-parser");
-
-
-app.use(flash());
-app.use(cookieParser());
-app.use(express.static("public"));
-app.use(session({ secret: "cats",resave: true, saveUninitialized: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(passport.session());
-
-require("./config/passport/passport.js")(passport, db.User);
-require("./controllers/authController.js")(app,passport);
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
 let exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(session({
+    secret: "yo mama",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {}
+}));
+
+app.use((req, res, next) => {
+    if (req.session.email) {
+        db.User.findOne({ where: { email: req.session.email } }).then(function (user) {
+            res.locals.user = {
+                id: user.id,
+                name: user.name
+            }
+            next();
+        })
+    }else{
+        next();
+    }
+    
+});
+
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 let getRoutes = require("./controllers/petController.js");
 let userRoutes = require("./controllers/userController.js");
